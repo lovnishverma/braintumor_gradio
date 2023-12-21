7cee7108ee5ee65fbfbaeb1495cce5b2966797fe
nielit ropar <nielitropar@users.noreply.huggingface.co>
2023-12-21 05:17:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import gradio as gr
-import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.applications.vgg16 import preprocess_input
-
-# Loading Models
-braintumor_model = load_model('models/brain_tumor_binary.h5')
 
 # Configure Streamlit
 st.set_page_config(
@@ -33,35 +25,6 @@ st.sidebar.info(
     "The model is based on VGG16 architecture."
 )
 
-# Function to preprocess the image
-def preprocess_image(img):
-    if isinstance(img, np.ndarray):
-        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    else:
-        img_bytes = img.read()
-        nparr = np.frombuffer(img_bytes, np.uint8)
-        img_gray = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-
-    img_processed = preprocess_imgs([img_gray], (224, 224))
-    return img_processed
-
-# Handle binary decision
-def binary_decision(confidence):
-    return 1 if confidence >= 0.5 else 0
-
-def predict_braintumor(img):
-    img_processed = preprocess_image(img)
-    pred = braintumor_model.predict(img_processed)
-    confidence = pred[0][0]
-    return "Brain Tumor Not Found!" if binary_decision(confidence) == 1 else "Brain Tumor Found!"
-
-def preprocess_imgs(set_name, img_size):
-    set_new = []
-    for img in set_name:
-        img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
-        set_new.append(preprocess_input(img))
-    return np.array(set_new)
-
 # Streamlit components below the Gradio interface
 uploaded_file = st.file_uploader("Choose an MRI image", type=["jpg", "jpeg"])
 
@@ -71,30 +34,11 @@ if uploaded_file is not None:
 
     # Perform prediction when the "Predict" button is clicked
     if st.button("Predict"):
-        img_array = preprocess_image(uploaded_file)
-        pred = braintumor_model.predict(img_array)
-        confidence = pred[0][0]
-        result = "Brain Tumor Not Found!" if binary_decision(confidence) == 1 else "Brain Tumor Found!"
+        # Perform your prediction logic here
+        # ...
 
-        # Display the prediction result with confidence
-        st.success(result)
-        st.markdown(f"Confidence: {confidence:.2%}")
-
-# Gradio interface
-iface = gr.Interface(
-    fn=predict_braintumor,
-    inputs="image",
-    outputs="text",
-    examples=[
-        ["examples/1_no.jpeg"],
-        ["examples/2_no.jpeg"],
-        ["examples/3_no.jpg"],
-        ["examples/Y1.jpg"],
-        ["examples/Y2.jpg"],
-        ["examples/Y3.jpg"],
-    ],
-    live=True
-)
-
-# Embed Gradio interface within Streamlit using st.write
-st.write(iface.launch())
+# Embed Gradio interface within Streamlit using an HTML iframe
+gradio_link = "http://localhost:7860/"
+st.markdown(f"### Gradio Interface")
+st.markdown(f"*(Note: Gradio Interface may take a moment to load.)*")
+st.markdown(f'<iframe src="https://huggingface.co/spaces/LovnishVerma/medical" width="100%" height="600px" frameborder="0"></iframe>', unsafe_allow_html=True)
