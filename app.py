45c37ad1294470d11cbce19487ad69a6a7c63f70
nielit ropar <nielitropar@users.noreply.huggingface.co>
2023-12-21 09:19:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -96,13 +96,27 @@ iface = gr.Interface(
     live=True
 )
 
-# Display Gradio interface
-st.markdown("<h1 style='text-align: center;'>Gradio Interface</h1>", unsafe_allow_html=True)
-st.markdown(
-    "<p style='text-align: center;'>This is an interactive interface powered by Gradio.</p>",
-    unsafe_allow_html=True
-)
-st.markdown("<hr>", unsafe_allow_html=True)
-
-# Display Gradio interface
-iface.launch()
+# Display Gradio interface within a custom block
+with st.markdown("<h1 style='text-align: center;'>Gradio Interface</h1>", unsafe_allow_html=True):
+    with st.markdown("<p style='text-align: center;'>This is an interactive interface powered by Gradio.</p>", unsafe_allow_html=True):
+        with gr.Blocks():
+            gr.HTML(
+                """
+                <div style="display: flex; justify-content: center; align-items: center; text-align: center;">
+                <a href="https://github.com/showlab/MotionDirector" style="margin-right: 20px; text-decoration: none; display: flex; align-items: center;">
+                </a>
+                <div>
+                    <h1>MotionDirector: Motion Customization of Text-to-Video Diffusion Models</h1>
+                    <h5 style="margin: 0;">More MotionDirectors are on the way. Stay tuned 🔥!</h5>
+                     <h5 style="margin: 0;"> If you like our project, please give us a star ✨ on Github for the latest update.</h5>
+                    </br>
+                    <div style="display: flex; justify-content: center; align-items: center; text-align: center;">
+                        <a href="https://arxiv.org/abs/2310.08465"></a>
+                        <a href="https://arxiv.org/abs/2310.08465"><img src="https://img.shields.io/badge/arXiv-2310.08465-b31b1b.svg"></a>&nbsp;&nbsp;
+                        <a href="https://showlab.github.io/MotionDirector"><img src="https://img.shields.io/badge/Project_Page-MotionDirector-green"></a>&nbsp;&nbsp;
+                        <a href="https://github.com/showlab/MotionDirector"><img src="https://img.shields.io/badge/Github-Code-blue"></a>&nbsp;&nbsp;
+                    </div>
+                </div>
+                </div>
+                """
+            )
